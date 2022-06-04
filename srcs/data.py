04b214ffe4f383eca6c64db7e0c270e55726429d
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-04 19:30:52 +0200

    feature/data_management almost done, should test now

--- srcs/data.py
+++ srcs/data.py
@@ -3,39 +3,43 @@ import random
 from movie import request_omdb
 
 class Game:
-	def __init__(self):
-		self.MovieMons = request_omdb() # a voir
-		self.playerStrength = 1.0 # a voir
-		self.position = (0, 0)
-		self.movieballsNb = 0
-		self.moviedex = []
+	def __init__(self, MovieMons, playerStrength, position, movieballsNb, moviedex):
+		self.MovieMons = MovieMons
+		self.playerStrength = playerStrength
+		self.position = position
+		self.movieballsNb = movieballsNb
+		self.moviedex = moviedex
 
 class Player:
 	def __init__(self):
-		self.MovieMons = request_omdb()
-		self.playerStrength = 1.0
+		self.load_default_settings()
 
 	#### member functiosn asked in the subject ####
 
 	def load(self, game):
+		self.MovieMons = game.MovieMons
+		self.playerStrength = game.playerStrength
 		self.position = game.position
 		self.movieballsNb = game.movieballsNb
 		self.moviedex = game.moviedex
 
 	def dump(self):
-		save = Game()
-		save.playerStrength = self.playerStrength
-		save.position = self.position
-		save.movieballsNb = self.movieballsNb
-		save.moviedex = self.moviedex
-		save.playerStrength = self.playerStrength
+		save = Game(self.MovieMons, 
+				self.playerStrength, 
+				self.position, 
+				self.movieballsNb, 
+				self.moviedex)
 		return save
 
 	def get_random_movie(self):
-		random.choice(self.MovieMons)
+		return random.choice(self.MovieMons).keys() # have to check later
 
 	def load_default_settings(self):
-		pass
+		self.MovieMons = request_omdb()
+		self.playerStrength = 3.0
+		self.position = (0, 0)
+		self.movieballsNb = 0
+		self.moviedex = []
 
 	def get_strength(self):
 		return self.playerStrength
