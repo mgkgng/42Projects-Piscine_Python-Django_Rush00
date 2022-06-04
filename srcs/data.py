f80025c55c04cb35a6091b9ef34954b0f834358e
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-04 19:13:28 +0200

    feature/data_management class Game created

--- srcs/data.py
+++ srcs/data.py
@@ -2,12 +2,19 @@ from this import d
 import random
 from movie import request_omdb
 
+class Game:
+	def __init__(self):
+		self.position = (0, 0)
+		self.movieballsNb = 0
+		self.moviedex = []
+		
+
 class Data:
 	def __init__(self):
 		self.MovieMons = request_omdb()
 		self.playerStrength = 1.0
 
-	def load(self):
+	def load(self, game):
 		pass
 
 	def dump(self):
