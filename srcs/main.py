1374ef98dc5679092af8f68c8126df92c0d0ecce
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-04 18:59:27 +0200

    feature/get_data done

--- srcs/main.py
+++ srcs/main.py
@@ -1,28 +1,33 @@
 import re
 import requests, json, sys
 
-def request_omdb(moviename):
+def request_omdb():
+	res = dict()
+
+	movielist = open("movielist.txt", 'r').readlines()
+	request_omdb(movielist)
+
 	S = requests.Session()
 	URL = "http://www.omdbapi.com/?i=tt3896198&apikey=c4f868ed"
 
 	R = S.get(url=URL)
 	DATA = R.json()
 
-	PARAMS = {
-		"t": moviename,
-		"type": "movie",
-		"r": "json",
-	}
+	for movie in movielist:
+		PARAMS = {
+			"t": movie,
+			"type": "movie",
+			"r": "json",
+		}
 
-	R = S.get(url=URL, params=PARAMS)
-	if (R.status_code == 404):
-		print("Movie not found.")
-		exit(1)
-	DATA = R.json()
-	return DATA["imdbRating"]
+		R = S.get(url=URL, params=PARAMS)
+		if (R.status_code == 404):
+			print("Error encountered while requesting to omdb api")
+			exit(1)
+		DATA = R.json()
+		res[movie] = DATA["imdbRating"]
 
+	return res
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		exit(0)
-	request_omdb(sys.argv[1])
+	request_omdb()
