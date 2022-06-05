3853cd6bc1939e54cc4a956078a242063685f910
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-05 14:32:31 +0200

    save

--- MovieMon/gameplay/views.py
+++ MovieMon/gameplay/views.py
@@ -3,35 +3,10 @@ from .srcs.worldmap import Worldmap
 
 worldmap = Worldmap(10, 10)
 
+def titlepage(request):
+    return render(request, "html/titlepage.html", {"path": titlepage})
+
 def gameboy(request):
+    print(request.GET.get('value'))
+    #form = Button()
     return render(request, "html/gameboy.html")
-
-def input(request):
-    pass
-
-def worldmap(request):
-    pass
-
-def buttonUp(request):
-    pass
-
-def buttonDown(request):
-    pass
-
-def buttonMiddleLeft(request):
-    pass
-
-def buttonMiddleRight(request):
-    pass
-
-def buttonA(request):
-    pass
-
-def buttonB(request):
-    pass
-
-def buttonStart(request):
-    pass
-
-def buttonSelect(request):
-    pass
